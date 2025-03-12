7a274a0bcd05230e5548f18d1027c881172d3be5
iamanandri <irfananandri@gmail.com>
2025-03-13 05:45:18 +0700

    Working around with data

--- main.go
+++ main.go
@@ -11,15 +11,15 @@ func main() {
 	//Check if array is empty
 	currentTask := ""
 
-	fmt.Scan(currentTask);
+	fmt.Print("Enter task into the list: ")
+	fmt.Scan(&currentTask)
 
-	list[0] = addNewTask(currentTask)
+	list[0] = currentTask
 
 	fmt.Print(list[0])
 
 }
 
-func addNewTask (currentTask string) string {
+func addNewTask(currentTask string) string {
 	return "Baboom"
 }
-
