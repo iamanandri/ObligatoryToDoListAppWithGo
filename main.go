456794abd709561f85e77f1949fb0e7660f84fc0
iamanandri <irfananandri@gmail.com>
2025-03-12 21:16:47 +0700

    Started scribbling down code, maps, functions

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("***** Another Obligatory To Do List App *****")
+	fmt.Println()
+	list := make(map[int]string)
+	//Check if array is empty
+	currentTask := ""
+
+	fmt.Scan(currentTask);
+
+	list[0] = addNewTask(currentTask)
+
+	fmt.Print(list[0])
+
 }
+
+func addNewTask (currentTask string) string {
+	return "Baboom"
+}
+
