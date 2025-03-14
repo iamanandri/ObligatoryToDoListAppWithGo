c057db0510d2d6234285f2f011eb8fc24db58859
iamanandri <irfananandri@gmail.com>
2025-03-14 09:23:42 +0700

    Added a command mechanism with maps

--- main.go
+++ main.go
@@ -2,24 +2,54 @@ package main
 
 import (
 	"fmt"
+	//"maps"
 )
 
 func main() {
 	fmt.Println("***** Another Obligatory To Do List App *****")
 	fmt.Println()
-	list := make(map[int]string)
+	//list := make(map[int][]string)
+	keyword := map[string][]any{
+		"help": {
+		},
+		"add": {
+			"task",
+			"function",
+		},
+		"remove": {
+			"task",
+		},
+		"update": {
+			"task",
+		},
+	}
 	//Check if array is empty
-	currentTask := ""
+	//currentTask := ""
+	
+	var command string
 
-	fmt.Print("Enter task into the list: ")
-	fmt.Scan(&currentTask)
+	fmt.Printf("What do you want to do? We have %d functions (Type \"help\" to get command list): ", len(keyword))
+	fmt.Scan(&command)
 
-	list[0] = currentTask
-
-	fmt.Print(list[0])
-
+	fmt.Println(keyword[command])
 }
 
-func addNewTask(currentTask string) string {
-	return "Baboom"
+func helpFunctions() string {
+	return "add - Add a new task\nremove - Remove a task\nupdate - Update a task"
+}
+
+func addTask(task string) string {
+	return "You added a " + task;
+}
+
+func removeTask() string {
+	return "You removed a task!"
+}
+
+func updateTask() string {
+	return "You updated a task!"
+}
+
+func returnNumber() int {
+	return 5
 }
